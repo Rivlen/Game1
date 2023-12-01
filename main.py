c428b8d02fcb18520f20fdc37f8fe3737ddd8243
Rivlen <x.stawecki@gmail.com>
2023-12-01 12:03:52 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+from random import randint
+
+
+def guess_the_number():
+    roll = randint(1, 100)
+    while True:
+        while True:
+            try:
+                guess = int(input("Guess the number: "))
+                break
+            except ValueError:
+                print("It's not a number!")
+        if guess < roll:
+            print("Too small!")
+            continue
+        elif guess > roll:
+            print("Too big!")
+        elif guess == roll:
+            print("You win!")
+            return None
+
+
+if __name__ == '__main__':
+    guess_the_number()
